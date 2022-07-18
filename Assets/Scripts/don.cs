23a20f57a7f160767058c2971403a45e8bdd45f4
akinaksoooy <akinaksoyiletisim@gmail.com>
2022-07-18 11:53:14 +0300

    aaOyunu dosyalari

---
+++ Assets/Scripts/don.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class don : MonoBehaviour
+{
+    [SerializeField]
+    float donmeHizi;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,0,donmeHizi * Time.deltaTime);
+        
+    }
+}
